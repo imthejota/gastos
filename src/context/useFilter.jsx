04df16a696b664cb141fc2a204ee3b20b554e178
Jonathan Kechian <jkechian@gmail.com>
2024-05-12 12:51:31 -0300

    Adding all as default value

--- src/context/useFilter.jsx
+++ src/context/useFilter.jsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 const context = (set) => ({
-  category: null,
+  category: "all",
   setCategory: (data) => set((state) => ({ ...state, category: data })),
   resetCategory: () => set((state) => ({ ...state, category: null })),
 });
